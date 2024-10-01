17ab387519513f918f1c03472cad364bc41a024c
TheBlaze <santi.volcanos@gmail.com>
2024-10-01 11:40:33 -0600

    Powerup Script and idle anim

---
+++ Assets/Scripts/PowerupScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupScript : MonoBehaviour
+{
+    [SerializeField] int powerUpType = 1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("Player")) {
+            Player hasPowerup=other.GetComponent<Player>();
+        if(powerUpType==1&&!hasPowerup.katana)
+        {
+            hasPowerup.katana = true;
+            gameObject.SetActive(false);
+        } 
+        else
+            { hasPowerup.bazooka = true;
+              gameObject.SetActive (false);}
+        }
+        
+        
+    }
+    private void Update()
+    {
+        if(powerUpType==1) {transform.Rotate(0, 10 * Time.deltaTime, 0); }
+        else
+        { transform.Rotate(10 * Time.deltaTime, 0, 0); }
+    }
+}
